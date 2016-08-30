b5f58487be91609c58804d1d6dbcb9a4a0cd2843
Maximiliano García Rozado <mrozado@lagash.com>
2016-08-30 11:03:24 -0300

    Console app

---
+++ IoTPOC/IoTPOC.DataReceiver.Console/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoTPOC.DataReceiver.ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string eventHubConnectionString = ConfigurationManager.AppSettings["eventHub:ConnectionString"];
+            string eventHubName = ConfigurationManager.AppSettings["eventHub:Name"];
+            string eventHubName2 = ConfigurationManager.AppSettings["eventHub:Name2"];
+            string storageConnectionString = ConfigurationManager.AppSettings["storageConnectionString"];
+
+            string eventProcessorHostName = Guid.NewGuid().ToString();
+            EventProcessorHost eventProcessorHost = new EventProcessorHost(eventProcessorHostName, eventHubName, EventHubConsumerGroup.DefaultGroupName, eventHubConnectionString, storageConnectionString);
+
+            Console.WriteLine("Registering EventProcessor...");
+            var options = new EventProcessorOptions();
+            options.ExceptionReceived += (sender, e) => { Console.WriteLine(e.Exception); };
+            eventProcessorHost.RegisterEventProcessorAsync<SimpleEventProcessor>(options).Wait();
+
+            Console.WriteLine("Receiving. Press enter key to stop worker.");
+            Console.ReadLine();
+            eventProcessorHost.UnregisterEventProcessorAsync().Wait();
+            //eventProcessorHost2.UnregisterEventProcessorAsync().Wait();
+        }
+    }
+}
